eb052358841f92eb774b029c2017232c3ccbda8c
Dy <gdy1218@gmail.com>
2020-06-06 16:24:34 +0200

    added searchbar and icons

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FileSearch from "./components/FileSearch";
+
+function App() {
+  return (
+    <div className="App container-fluid">
+      <div className="row">
+        <div className="col bg-danger left-panel">
+          <FileSearch
+            title="My document"
+            onFileSearch={(value) => {
+              console.log(value);
+            }}
+          />
+        </div>
+        <div className="col bg-primary right-panel">
+          <h1>this is the right</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
